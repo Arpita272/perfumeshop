4e200fd67ef4b69a8c34d19b9c140625667434a2
Arpita272 <arpitadevaraya272@gmail.com>
2025-11-03 13:44:32 +0000

    added html and css files from responsive demo to react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import mainimage from './productimages/mainimage.avif';
+import citrusgarden from './productimages/citrusgarden.avif';
+import honeykiss from './productimages/honeykiss.avif';
+import jasminebreeze from './productimages/jasminebreeze.avif';
+import lavenderfields from './productimages/lavenderfields.avif';
 
-function App() {
+export default function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <div className="shippingtext">
+        <p>FREE SHIPPING OVER £50 WORLDWIDE</p>
+      </div>
+
+      <div className={`tabs ${menuOpen ? "responsive" : ""}`} id="myTabs">
+        <a href="#maintab" className="maintab">NOUS DEUX FRAGRANCES</a>
+        <a href="#subtab" className="subtab">SHOP ALL</a>
+        <a href="#subtab" className="subtab">BODY</a>
+        <a href="#subtab" className="subtab">HOME SCENTS</a>
+
+        <i className="fa-solid fa-cart-shopping fa-lg"></i>
+        <i className="fa-solid fa-circle-user fa-lg"></i>
+
+        <a href="#!" className="icon" onClick={toggleMenu}>
+          <i className="fa fa-bars"></i>
         </a>
-      </header>
-    </div>
+      </div>
+
+      <div className="body1">
+        <div className="text">
+          <p className="title">NOMAD 13 EAU DE PARFUM</p>
+          <p className="des">Discover the original fragrance</p>
+          <button>Shop now</button>
+        </div>
+
+        <div className="mainimage">
+          <img src={mainimage} alt="main" />
+        </div>
+      </div>
+
+      <div className="bestsellers">
+        <h4>BEST SELLERS</h4>
+        <div className="viewmore">
+          <p>View More</p>
+          <i className="fa-solid fa-arrow-right"></i>
+        </div>
+      </div>
+
+      <div className="catalogue">
+        <div className="catalogueimages">
+
+          <div className="imgbox">
+            <img src={lavenderfields} alt="lavender" />
+            <span className="label">Best Seller</span>
+            <p className="desc">LAVENDER FIELD, 100 ml/Home Fragrance</p>
+            <p className="rate">£85.00</p>
+          </div>
+
+          <div className="imgbox">
+            <img src={honeykiss} alt="honeykiss" />
+            <span className="label">Best Seller</span>
+            <p className="desc">HONEY KISS, 100 ml/Body Lotion</p>
+            <p className="rate">£150.00</p>
+          </div>
+
+          <div className="imgbox">
+            <img src={citrusgarden} alt="citrus" />
+            <span className="label">Best Seller</span>
+            <p className="desc">CITRUS GARDEN, Candle 250g/8.6 oz</p>
+            <p className="rate">£55.00</p>
+          </div>
+
+          <div className="imgbox">
+            <img src={jasminebreeze} alt="jasmine" />
+            <span className="label">Best Seller</span>
+            <p className="desc">JASMINE BREEZE, 100 ml/Eau De Parfum</p>
+            <p className="rate">£180.00</p>
+          </div>
+
+        </div>
+       </div>
+    </>
   );
 }
-
-export default App;
